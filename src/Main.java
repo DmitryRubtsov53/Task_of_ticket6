6ba613c4235301f19ad19e8df3b615101d8daf0f
DmitryRubtsov53 <2578390@mail.ru>
2022-10-03 22:15:48 +0500

    Ренение задачи Билета 6.

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задание к экзаменационному билету № 6 1-го курса Java");
+
+        int[] arr = {0,1,2,3,4,5,6,7,8,9,10,11,12};
+        System.out.println("\n" + "Исходный массив: " + Arrays.toString(arr));
+        System.out.println("Тербуется: Все четные числа увеличить на единицу. " +
+                "\n" + "Возвратить кусок списка с 3-го по 7-й элемент включительно." );
+
+        System.out.println ("Результат: " + makeList3to7 (arr));  // Вызов метода
+
+    } // скобка метода main
+
+    public static String makeList3to7 (int[] arr) {
+        int i = 0;
+        int[] oddArr = new int [arr.length];
+
+        for (int element : arr) {
+            if (element % 2 != 0) {
+                oddArr[i] = element;
+            } else { oddArr[i] = element + 1; }
+            i++;
+        }
+/*      *********************************************************************************************************
+        String s = Arrays.toString (oddArr);
+        System.out.println(s);             на консоль выводится: [1, 1, 3, 3, 5, 5, 7, 7, 9, 9, 11, 11, 13]
+        System.out.println(s.substring (2, 6));
+        return s.substring (2, 7);
+        Такой способ не подходит, т.к. s.substring (2, 7) считает элементами и цифры, и пробелы, и запятые !!!
+        Результат :  ", 1, 3"
+        *********************************************************************************************************
+*/
+        i = 0;
+        int[] arr3to7 = new int[5];
+        for (int j = 2; j < 7; j++) {
+            arr3to7[i] = oddArr[j];
+            i++;
+        }
+        return Arrays.toString (arr3to7);
+
+    }
+
+} // скобка class Main
